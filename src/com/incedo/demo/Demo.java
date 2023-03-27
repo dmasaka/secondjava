bbde0a1c4852124ca9a561e6d7ab67eaf30fcd96
David Masaka <david.masaka@incedoinc.com>
2023-03-27 13:35:05 -0400

    added all the intial files

---
+++ src/com/incedo/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.incedo.demo;
+
+/**
+ * @author David Masaka
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first Java Program");
+	}
+
+}
